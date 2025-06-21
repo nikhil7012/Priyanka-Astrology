275c69cee6179f597a641fe4d8bda61e0590e984
nikhil7012 <vanish555vs@gmail.com>
2025-06-20 23:36:40 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-4xl font-bold text-purple-700">Priyanka Sharma</h1>
+      <p className="text-lg mt-4">Welcome to my astrology website ✨🔮</p>
+    </div>
+  );
+}
+
+export default App;
