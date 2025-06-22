1bd8018dd2cdb230523fbff9fbddf864b106bebd
nikhil7012 <vanish555vs@gmail.com>
2025-06-21 21:56:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from "react";
-
 function App() {
   return (
     <div className="min-h-screen bg-white text-center p-8">
@@ -9,12 +8,12 @@ function App() {
 
       {/* Crystal Ball Icon as Button */}
       <div className="mt-6">
-        <button
-          onClick={() => window.open("/astro-details.html", "_blank")}
-          className="text-4xl hover:scale-110 transition-transform duration-300"
-        >
-          🔮
-        </button>
+       <button
+  onClick={() => window.open("/astro-details.html", "_blank")}
+  className="text-4xl hover:scale-110 transition-transform duration-300"
+>
+  🔮
+</button>
       </div>
     </div>
   );
